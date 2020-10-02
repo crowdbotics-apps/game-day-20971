9ccdcfdecc4a6a3d4f0a856481a6e20204ad6a9b
Crowdbotics <team@crowdbotics.com>
2020-10-02 12:30:50 +0000

    Update models

--- backend/game_day_20971/wsgi.py
+++ backend/game_day_20971/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game_day_20971.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "game_day_20971.settings")
 
 application = get_wsgi_application()
