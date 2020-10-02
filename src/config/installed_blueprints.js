2ed8499479babb1af81ed0615fa29363b5092491
Crowdbotics <team@crowdbotics.com>
2020-10-02 12:31:58 +0000

    add package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'UserProfile121467', human_name: 'User Profile', access_route: 'UserProfile121467'},
 { name: 'Settings121466', human_name: 'Settings', access_route: 'Settings121466'},
 { name: 'Settings121464', human_name: 'Settings', access_route: 'Settings121464'},
 { name: 'SignIn2121462', human_name: 'Sign In 2', access_route: 'SignIn2121462'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import UserProfile121467Navigator from '../features/UserProfile121467/navigator';
 import Settings121466Navigator from '../features/Settings121466/navigator';
 import Settings121464Navigator from '../features/Settings121464/navigator';
 import SignIn2121462Navigator from '../features/SignIn2121462/navigator';
@@ -17,6 +18,7 @@ import BlankScreen0121460Navigator from '../features/BlankScreen0121460/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+UserProfile121467: { screen: UserProfile121467Navigator },
 Settings121466: { screen: Settings121466Navigator },
 Settings121464: { screen: Settings121464Navigator },
 SignIn2121462: { screen: SignIn2121462Navigator },
